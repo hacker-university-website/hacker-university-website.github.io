56cd988d6cc29a31c2d3f852da113827aa4de0cc
rkdesignio <jaemnkm@gmail.com>
2017-10-22 20:56:45 -0700

    added edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,3 +98,28 @@
 	});
 
 })(jQuery);
+
+
+
+
+(function() {
+  var boxes = document.getElementsByClassName("image-box");
+  
+  Array.prototype.forEach.call(boxes, function(box) {
+    var detail = box.getElementsByClassName("image-detail")[0];
+    var img = box.getElementsByTagName("img")[0];
+    var btn = box.getElementsByClassName("button")[0];
+    
+    box.addEventListener("mouseover", function() {
+      detail.classList.add("hide");
+      img.classList.add("detail-hover-state");
+      btn.classList.add("show");
+    });
+    
+    box.addEventListener("mouseout", function() {
+      detail.classList.remove("hide");
+      img.classList.remove("detail-hover-state");
+      btn.classList.remove("show");
+    });
+  });
+})();
